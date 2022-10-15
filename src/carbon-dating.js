bb4585b59154f658d54fcaa9090ec594fe1d3ceb
Glockers <megamaxchanel@gmail.com>
2022-10-16 00:11:39 +0300

    feat: carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,25 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  return sampleActivity > 0 &&
+    sampleActivity <= 15 &&
+    (typeof sampleActivity === 'string')? Math.ceil( Math.log( MODERN_ACTIVITY/ sampleActivity)/(0.693/HALF_LIFE_PERIOD)): false
+
+  // let result  = Number(sampleActivity) || false
+
+  // if(!result ) return false
+
+  // if(result<=0 || result>15 || result === NaN ){
+    // return false
+  // }
+  // return Math.ceil(Math.log(MODERN_ACTIVITY / result) / (0.693 / HALF_LIFE_PERIOD));
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
+
+// console.log(dateSample('3'))
+
 
 module.exports = {
   dateSample
