0b5f82dbc36d795e3f5509286f589482f028492e
Glockers <megamaxchanel@gmail.com>
2022-10-16 17:22:56 +0300

    feat: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+function copyNext(index, arr){
+  let nextValue = arr[index+1]
+  if(!nextValue) {arr.splice(index, 1); return};
+  arr[index] = nextValue
+
 }
+
+function copyPrev(index, arr){
+  let prevValue = arr[index-1]
+  if(!prevValue){arr.splice(index, 1); return};
+  arr[index] = prevValue
+}
+
+
+function removePrev(index, arr){
+  let prevValue = arr[index-1]
+  if(!prevValue) {arr.splice(index, 1); return};
+  arr.splice(index, 1)
+  console.log(arr, index)
+  delete arr[index-1]
+}
+
+function removeNext(index, arr){
+  let nextValue = arr[index+1]
+  if(!nextValue ){arr.splice(index, 1); return};
+  arr.splice(index, 1)
+  delete arr[index]
+}
+
+
+function transform(array) {
+  
+  try {
+    
+    if(!Array.isArray(array)) throw "'arr' parameter must be an instance of the Array!"
+    let arr = Object.assign([], array)
+    
+
+    const control_sequences = {"--discard-next": removeNext, "--discard-prev": removePrev, "--double-next":copyNext , "--double-prev": copyPrev}
+    const objectKeys = Object.keys(control_sequences)
+    array.forEach(elementArr => {
+      if(typeof elementArr == "string" && objectKeys.includes(elementArr)){
+        const index = arr.findIndex(value => value == elementArr);
+        control_sequences[elementArr](index, arr)
+      }
+    });
+    
+  return arr.filter(Boolean)
+
+  } catch (error) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+  
+
+}
+console.log(transform([1, 2, 3, '--double-next', 1337, '--double-prev', 4, 5]))
+
 
 module.exports = {
   transform
