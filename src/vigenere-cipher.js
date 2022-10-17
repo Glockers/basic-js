d4151a0ce2365188cb45b968a78f295465cf1060
Glockers <megamaxchanel@gmail.com>
2022-10-17 12:33:28 +0300

    add: cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+  constructor(type = true) {
+    this.type = type;
+    this.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  cipher(string, key, method){
+    string = string.toUpperCase().split("");
+    key = key.toUpperCase().split("");
+    let wordArray = [];
+    let iterKey = 0;
+    for (let iterator = 0; iterator < string.length; iterator++) {
+      let strIndex = this.alphabet.indexOf(string[iterator]);
+      if (strIndex === -1) {
+        wordArray.push(string[iterator]);
+        continue;
+      }
+        if (iterKey >= key.length) iterKey = iterKey % key.length;
+        let newLetter = method ? this.alphabet[((this.alphabet.length + (strIndex + this.alphabet.indexOf(key[iterKey]))) % this.alphabet.length)] : this.alphabet[(this.alphabet.length + (strIndex - this.alphabet.indexOf(key[iterKey]))) %this.alphabet.length];
+        wordArray.push(newLetter);
+        iterKey++;
+    }
+
+    if (this.type) {
+      return wordArray.join("");
+    }
+    return wordArray.reverse().join("");
+  }
+
+  encrypt(string, key) {
+    if (!string || !key) {
+			throw new Error('Incorrect arguments!');
+		}
+    return this.cipher(string, key, true)
+  }
+
+  decrypt(string, key) {
+    if (!string || !key) {
+			throw new Error('Incorrect arguments!');
+		}
+    return this.cipher(string, key, false)
   }
 }
 
