b9937f1ec84b8689cc70381685b77c74a0699bbc
Glockers <megamaxchanel@gmail.com>
2022-10-17 11:05:47 +0300

    feat: extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options ) {
+
+  options.addition = options.addition === null || options.addition ? String(options.addition) : options.addition
+  
+  options.repeatTimes = options.repeatTimes? options.repeatTimes : 1
+  options.additionRepeatTimes = options.additionRepeatTimes? options.additionRepeatTimes : 1
+
+  let arrayString = []
+  for(let i=0; i<options.repeatTimes;  i++){
+    let buffer = []
+    for(let j=0; j< options.additionRepeatTimes;j++){
+      buffer.push(options.addition)
+    }
+
+    arrayString.push(str + buffer.join(options.additionSeparator|| "|"))
+    console.log(buffer, options.additionRepeatTimes , arrayString, options.repeatTimes)
+
+  }
+
+  return arrayString.join(options.separator || "+")
 }
+
+console.log(repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }))
+console.log(repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }) == "nullnull!!!null!!!null??? nullnull!!!null!!!null??? nullnull!!!null!!!null")
+
 
 module.exports = {
   repeater
